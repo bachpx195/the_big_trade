60f82536b7f6fd47cae747e3a8faf844335a6cfb
bachpx195 <bachpx195@gmail.com>
2021-02-15 19:25:52 +0700

    chen thanh

--- python_for_finance/models/candlestick.py
+++ python_for_finance/models/candlestick.py
@@ -2,7 +2,7 @@ import pandas as pd
 from database.pymysql_conn import DataBase
 
 db = DataBase()
-SQL = "SELECT * FROM DailyTradingJournal_development.candlesticks;"
+SQL = "SELECT *  FROM DailyTradingJournal_development.candlesticks  ORDER BY candlesticks.date ASC;"
 
 
 class Candlestick:
@@ -11,9 +11,10 @@ class Candlestick:
 
     def to_df(self):
         db.cur.execute(SQL)
-        columns = ['date', 'open', 'high', 'close', 'low', 'volumn']
+        columns = ['date', 'open', 'high', 'close', 'low', 'volumn', 'atr14']
         datas = list(db.cur.fetchall())
-        data = [(da[8], da[3], da[4], da[5], da[6], da[11])for da in datas]
+        data = [(da[8], da[3], da[4], da[5], da[6], da[11], da[7])
+                for da in datas]
         df = pd.DataFrame(columns=columns, data=data)
-        df = df.set_index('date')
+        # df = df.set_index('date')
         return df
