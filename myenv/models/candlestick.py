1ed6e50a0846e8cdf7df16967da83e2e55475df8
bachpx195 <bachpx195@gmail.com>
2021-07-17 14:05:12 +0700

    update

--- myenv/models/candlestick.py
+++ myenv/models/candlestick.py
@@ -3,14 +3,16 @@ from myenv.database.pymysql_conn import DataBase
 
 db = DataBase()
 SQL = "SELECT *  FROM DailyTradingJournal_development.candlesticks  ORDER BY candlesticks.date ASC;"
+INTERVAL_HASH = {"day": 1, "week": 2}
 
 
 class Candlestick:
-    def __init__(self):
-        self.sql = SQL
+    def __init__(self, interval="day"):
+        self.interval = interval
 
     def to_df(self):
-        db.cur.execute(SQL)
+        sql_query = f"SELECT *  FROM DailyTradingJournal_development.candlesticks WHERE candlesticks.time_type = {INTERVAL_HASH[self.interval]} ORDER BY candlesticks.date ASC;"
+        db.cur.execute(sql_query)
         columns = ['date', 'open', 'high', 'close', 'low']
         datas = list(db.cur.fetchall())
         data = [(da[8], da[3], da[4], da[5], da[6])
