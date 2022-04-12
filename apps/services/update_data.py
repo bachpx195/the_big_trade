222a17933e30f94d1a4cf46311bfc90a001a74f2
Bachpx <bachpx195@gmail.com>
2022-04-12 07:54:46 +0700

    update

--- apps/services/update_data.py
+++ apps/services/update_data.py
@@ -1,9 +1,9 @@
 import subprocess
 
-def update_data(merchandise_rate, interval):
+def update_data(merchandise_rate, interval, quote='USDT'):
   base = merchandise_rate.split('USDT')[0]
 
-  cmd = f"cd && cd Projects/daily_trading_journal/ && bundle exec rake db:import_candlestick_data base={base} quote=USDT interval={interval}"
+  cmd = f"cd && cd Projects/daily_trading_journal/ && bundle exec rake db:import_candlestick_data base={base} quote={quote} interval={interval}"
   print(cmd)
   ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
   while True:
@@ -29,4 +29,17 @@ def update_all_data(merchandise_rate):
   is_updated_day = update_data(merchandise_rate, 'day')
   print("update hour")
   is_updated_hour = update_data(merchandise_rate, 'hour')
-  return is_updated_month and is_updated_week and is_updated_day and is_updated_hour
+
+  is_updated_with_btc_quote = True if merchandise_rate == "BTCUSDT" else False
+  if merchandise_rate != "BTCUSDT":
+    print("update month with BTC quote")
+    is_updated_month_btc = update_data(merchandise_rate, 'month', 'BTC')
+    print("update week with BTC quote")
+    is_updated_week_btc = update_data(merchandise_rate, 'week', 'BTC')
+    print("update day with BTC quote")
+    is_updated_day_btc = update_data(merchandise_rate, 'day', 'BTC')
+    print("update hour with BTC quote")
+    is_updated_hour_btc = update_data(merchandise_rate, 'hour', 'BTC')
+    is_updated_with_btc_quote = is_updated_month_btc and is_updated_week_btc and is_updated_day_btc and is_updated_hour_btc
+
+  return is_updated_month and is_updated_week and is_updated_day and is_updated_hour and is_updated_with_btc_quote
